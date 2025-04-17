cbc99bda3b8564858a38587152f589d7a6614ae6
Trend2Zero Dev <dev@trend2zero.com>
2025-04-17 15:20:33 -0700

    Rebuild Next.js cache and static files

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -265,7 +265,6 @@ export default function Home() {
     checkApi();
     fetchMarketData();
   }, []);
-  
   // Update market data from WebSocket stream
   useEffect(() => {
     if (wsMarketData) {
